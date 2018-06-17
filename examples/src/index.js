6368830c6d54f41fcf81a891f720bf648d5968ff
flyingalex <hulinalex@163.com>
2018-06-17 13:59:00 +0800

    add npm package config

---
+++ examples/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from 'react-dom';
+import IntlTest from './IntlTest';
+import IntlProvider from '../../src/index';
+
+const styles = {
+  textAlign: 'center',
+};
+
+const App = () => (
+  <div style={styles}>
+    <IntlTest name="CodeSandbox" />
+  </div>
+);
+
+// languages text config
+const en = {
+  test: 'hello {name}',
+};
+const zh = {
+  test: '你好',
+};
+
+const languages = {
+  en,
+  zh,
+};
+
+render(
+  <IntlProvider languages={languages} init="en">
+    <App />
+  </IntlProvider>,
+  document.getElementById('root'),
+);
--- src/index.js
+++ src/index.js
@@ -1,34 +1,76 @@
 import React from 'react';
-import { render } from 'react-dom';
-import IntlTest from './IntlTest';
-import IntlProvider from './IntlProvider';
+import { PropTypes } from 'prop-types';
 
-const styles = {
-  textAlign: 'center',
+const IntlContext = React.createContext();
+
+// put this at the top of the app
+class IntlProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    const { languages, init } = this.props;
+    if (!Object.keys(languages).includes(init)) {
+      throw new Error(`no such language[${init}]`);
+    }
+    this.state = {
+      locale: languages[init],
+      langs: Object.keys(languages),
+    };
+  }
+
+  changeLan = locale => {
+    const { langs } = this.state;
+    const { languages } = this.props;
+    if (langs.includes(locale)) {
+      this.setState({ locale: languages[locale] });
+    } else {
+      throw new Error(`no such language[${locale}]`);
+    }
+  };
+
+  render() {
+    const { locale, langs } = this.state;
+    const { children } = this.props;
+    return <IntlContext.Provider value={{ langs, locale, changeLan: this.changeLan }}>{children}</IntlContext.Provider>;
+  }
+}
+
+IntlProvider.propTypes = {
+  children: PropTypes.object,
+  languages: PropTypes.object,
+  init: PropTypes.string,
 };
 
-const App = () => (
-  <div style={styles}>
-    <IntlTest name="CodeSandbox" />
-  </div>
-);
-
-// languages text config
-const en = {
-  test: 'hello {name}',
+const FormatMsg = props => {
+  const { id, inject = {} } = props;
+  return (
+    <IntlContext.Consumer>
+      {intl => {
+        let text = intl.locale[id];
+        const injectKeys = Object.keys(inject);
+        if (text === undefined) {
+          throw new Error(`no such id(${id})`);
+        }
+        text = text.replace(/{(.*?)}/g, matched => {
+          const key = matched.replace('{', '').replace('}', '');
+          if (inject[key]) {
+            return inject[key];
+          }
+          return matched;
+        });
+        return text;
+      }}
+    </IntlContext.Consumer>
+  );
 };
-const zh = {
-  test: '你好',
+
+const DefineLangue = props => {
+  return (
+    <IntlContext.Consumer>
+      {intl => <div onClick={() => intl.changeLan(props.locale)}>{props.children}</div>}
+    </IntlContext.Consumer>
+  );
 };
 
-const languages = {
-  en,
-  zh,
-};
-
-render(
-  <IntlProvider languages={languages} init="en">
-    <App />
-  </IntlProvider>,
-  document.getElementById('root'),
-);
+// apis
+export default IntlProvider;
+export { FormatMsg, DefineLangue };
